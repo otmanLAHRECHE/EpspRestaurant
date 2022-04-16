aef2b45bd2a78e50765842224bf585fc4b964be6
Otman LAHRECHE <laotman.47@gmail.com>
2022-04-16 12:14:44 +0100

    updates

---
+++ custom_widgets.py
@@ -0,0 +1,30 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QWidget
+
+
+class ChoseProduct(QWidget):
+    def __init__(self):
+        super(ChoseProduct, self).__init__()
+
+        layout = QtWidgets.QHBoxLayout()
+        self.chose_product = QtWidgets.QComboBox()
+
+        layout.setStretch(1)
+        layout.addWidget(self.chose_product)
+
+        self.setLayout(layout)
+
+
+class ChoseProductQte(QWidget):
+    def __init__(self):
+        super(ChoseProductQte, self).__init__()
+
+        layout = QtWidgets.QHBoxLayout()
+        self.chose_product_qte = QtWidgets.QDoubleSpinBox
+
+        layout.setStretch(1)
+        layout.addWidget(self.chose_product_qte)
+
+        self.setLayout(layout)
+
+
