0dc5d8393e9cfa0a62d6eb8474b495699365c9e3
Otman LAHRECHE <laotman.47@gmail.com>
2022-04-20 13:20:47 +0100

    updates

--- tools.py
+++ tools.py
@@ -3,5 +3,8 @@
 
 def forming_date(db_date):
     d = db_date.split("/")
-    new_date = d[2] + "-" + d[1] + "-" + d[0]
+    if len(d[1]) > 1:
+        new_date = d[2] + "-" + d[1] + "-" + d[0]
+    else:
+        new_date = d[2] + "-" + "0" + d[1] + "-" + d[0]
     return new_date
